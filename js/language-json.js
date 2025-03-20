989ba5984e846b7ef6b4ef576e2815ded9beaa59
darkosto0 <152528354+darkosto0@users.noreply.github.com>
2025-03-20 19:05:06 +0100

    translation working on main page

---
+++ js/language-json.js
@@ -0,0 +1,21 @@
+let translations = {};
+
+async function loadTranslations() {
+    const response = await fetch("../translation.json");
+    translations = await response.json();
+}
+
+function changeLanguage(lang) {
+    localStorage.setItem("selectedLanguage", lang);
+    document.querySelectorAll("[data-translate]").forEach(el => {
+        const key = el.getAttribute("data-translate");
+        if (translations[lang] && translations[lang][key]) {
+            el.innerHTML = translations[lang][key];
+        }
+    });
+}
+
+loadTranslations().then(() => {
+    const savedLang = localStorage.getItem("selectedLanguage") || "en";
+    changeLanguage(savedLang);
+});
